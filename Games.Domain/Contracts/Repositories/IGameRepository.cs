423b58a5cf286e707a0dd588790e324e3c1601c2
gabriel-paulino <gabriel.paulino104@gmail.com>
2021-11-02 11:53:07 -0300

    Game api: Definição dos contratos

---
+++ Games.Domain/Contracts/Repositories/IGameRepository.cs
@@ -0,0 +1,17 @@
+﻿using Games.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Games.Domain.Contracts.Repositories
+{
+    public interface IGameRepository : IDisposable
+    {
+        Task<IEnumerable<Game>> GetAll(int page, int quantity);
+        Task<Game> Get(Guid id);
+        Task<IEnumerable<Game>> Get(string name, string producer);
+        Task<Game> Create(Game game);
+        Task<Game> Update(Game game);
+        Task Delete(Guid id);
+    }
+}
---
+++ Games.Domain/Contracts/Services/IGameService.cs
@@ -0,0 +1,17 @@
+﻿using Games.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Games.Domain.Contracts.Services
+{
+    interface IGameService : IDisposable
+    {
+        Task<IEnumerable<Game>> GetAll(int page, int quantity);
+        Task<Game> Get(Guid id);
+        Task<Game> Create(Game jogo);
+        Task<Game> Update(Guid id, Game jogo);
+        Task<Game> UpdatePrice(Guid id, decimal price);
+        Task Delete(Guid id);
+    }
+}
