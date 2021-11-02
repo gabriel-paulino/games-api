f9763c7b85f005cf55aceddebd1cce4bd2210b08
gabriel-paulino <gabriel.paulino104@gmail.com>
2021-11-02 12:06:02 -0300

    Game api: Estruturação inicial da camada de Infra

---
+++ Games.Infra.Data/Repositories/GameRepository.cs
@@ -0,0 +1,60 @@
+﻿using Games.Domain.Contracts.Repositories;
+using Games.Domain.Entities;
+using Games.Infra.Data.Session;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Games.Infra.Data.Repositories
+{
+    public class GameRepository : IGameRepository
+    {
+        private readonly DbSession _dbSession;
+        private bool _disposed = false;
+
+        public GameRepository(DbSession dbSession)
+        {
+            _dbSession = dbSession;
+        }
+
+        ~GameRepository() =>
+            Dispose();
+
+        public Task<Game> Create(Game game)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Delete(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Game> Get(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Game>> Get(string name, string producer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Game>> GetAll(int page, int quantity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Game> Update(Game game)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            if (!_disposed)
+                _dbSession.Dispose();
+            GC.SuppressFinalize(this);
+        }
+    }
+}
---
+++ Games.Infra.Data/Session/DbSession.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Games.Infra.Data.Session
+{
+    public class DbSession : IDisposable
+    {
+        private readonly IConfiguration _configuration;
+
+        public DbSession(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        private Guid _id;
+        public IDbConnection Connection { get; }
+        public IDbTransaction Transaction { get; set; }
+
+        public DbSession()
+        {
+            _id = Guid.NewGuid();
+            Connection = new SqlConnection(_configuration.GetConnectionString("GameApiConnectionString"));
+            Connection.Open();
+        }
+        public void Dispose() => Connection?.Dispose();
+    }
+}
